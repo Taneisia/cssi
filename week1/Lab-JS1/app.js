2cb1aedbaf36086432a48b67421ceff055774d6b
taneisia <taneisiatheflash@gmail.com>
2018-07-31 09:44:14 -0400

    JavaScriptQuizPre.txt

--- week1/Lab-JS1/app.js
+++ week1/Lab-JS1/app.js
@@ -1,3 +1,13 @@
 function sendAlert() {
-  alert("Hello everyone, hello from CSSI@MECS");
+  console.log("Hello CSSI");
+var name1 = 'Bob';
+let name2 = 'Bill';
+const name3 = 'Bo';
+
+// reassinging....
+name1 = 'Amy';
+name2 = 'Aice';
+console.log(name1);
+console.log(name2);
+console.log(name3);
 }
