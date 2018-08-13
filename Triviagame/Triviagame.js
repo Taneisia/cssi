b5cb17c1a99413c7da0ef09cc2c54b6f5334d220
taneisia <taneisiatheflash@gmail.com>
2018-08-13 16:50:13 -0400

    Trivia game

---
+++ Triviagame/Triviagame.js
@@ -0,0 +1,18 @@
+// Button for first MainPage
+
+let redButton = document.querySelector('#red');
+redButton.addEventListener('click', (e) => {
+  document.getElementById("responseBox").style.backgroundColor="red";
+    document.getElementById("responseBox").innerHTML ="red";
+
+#blue buttonn
+
+let blueButton = document.querySelector('#resd');
+blueButton.addEventListener('click', (e) => {
+
+}
+#greenButton
+let greenButton = document.querySelector('#green');
+greenButton = document.querySelector('click', (e) => {
+
+}
