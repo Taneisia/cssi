2a4cccde83568746ae04315dff02263019d85067
taneisia <taneisiatheflash@gmail.com>
2018-08-14 12:26:30 -0400

    waffle

--- Triviagame/Triviagame.js
+++ Triviagame/Triviagame.js
@@ -2,12 +2,13 @@
 
 let redButton = document.querySelector('#red');
 redButton.addEventListener('click', (e) => {
-  document.getElementById("responseBox").style.backgroundColor="red";
-    document.getElementById("responseBox").innerHTML ="red";
+  document.getElementById("").style.backgroundColor="red";
+    document.getElementById("").innerHTML ="red"
+}
 
 #blue buttonn
 
-let blueButton = document.querySelector('#resd');
+let blueButton = document.querySelector('#blue');
 blueButton.addEventListener('click', (e) => {
 
 }
@@ -16,3 +17,40 @@ let greenButton = document.querySelector('#green');
 greenButton = document.querySelector('click', (e) => {
 
 }
+</script>
+// new code for button
+id= demo
+<button onclick="myFunction()"</button>
+
+
+  <element onclick="myScript">
+
+
+
+//Disney movie guestions
+
+//  1. Who is Simba's father?
+//a.Mufasa
+//b.Scar
+//c.
+//d.
+
+//  2.How did Cinderalla lose her glass slipper?
+//a. she gave it away.
+//b. she never had a glass slipper.
+//c. she ran and it fell off her foot.
+//d. Someone stole it.
+
+//  3. Why did Mulan join the military?
+//a. she wanted to prove that she can fight like a man.
+//b. Restore honor to her family's name.
+//c. To fall in love with a soldier.
+//d. To run away from her family.
+
+//  4. How did Hiccup lose a leg?
+//a. He was born without one.
+//b. He lost by trying to save toothless.
+//c. It got cut off because he disrespected his father.
+//d. It broke off of his leg.
+
+//5. 
